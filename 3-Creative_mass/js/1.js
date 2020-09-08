3d3b30d49c8c1a9b06d0ca32fe33715a4c308d32
Ruslana Piasetska <rpiasetska@gmail.com>
2020-09-08 16:50:46 +0300

    image updated

--- 3-Creative_mass/js/1.js
+++ 3-Creative_mass/js/1.js
@@ -1,7 +1,3 @@
-/* Put it right after body */
-/* Set id="mainGalleryImage" for '.gallery-main-image' */
-/* And set id="galleryCollection" for '.gallery-images' */
-
 const mainImage = document.getElementById('mainGalleryImage');
 const galleryCollection = document.getElementById('galleryCollection');
 

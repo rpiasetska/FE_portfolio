ba1ec344590f767c8e288dc7cf614fc789337578
Ruslana Piasetska <rpiasetska@gmail.com>
2020-09-12 00:39:54 +0300

    added image switcher

--- 3-Creative_mass/js/1.js
+++ 3-Creative_mass/js/1.js
@@ -3,5 +3,7 @@ const galleryCollection = document.getElementById('galleryCollection');
 
 galleryCollection.addEventListener('click', function(event) {
     if (!event.target.src) return false;
+    let tmp = mainImage.src; 
     mainImage.src = event.target.src;
+    event.target.src = tmp;
 });
